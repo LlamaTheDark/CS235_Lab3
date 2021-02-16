336e762d2b6d893ec68dd0dd382e97707687bbf3
LlamaTheDark <llamathedark@gmail.com>
2021-02-16 01:29:05 -0700

    apparently you can't change the array or the check.sh gets really sad so I'm fixing that now.

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -88,7 +88,12 @@ void Pathfinder::createRandomMaze() { // WORKING
 }
 
 std::vector<std::string> Pathfinder::solveMaze() {
-    if(!isValidMaze() || !findPath(0, 0, 0)){
+    int m[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE];
+    for(int i = 0; i < std::pow(MAZE_SIZE, 3); i++){
+        m[X(i, MAZE_SIZE)][Y(i, MAZE_SIZE)][Z(i, MAZE_SIZE)] = m[X(i, MAZE_SIZE)][Y(i, MAZE_SIZE)][Z(i, MAZE_SIZE)];
+    }
+
+    if(!isValidMaze() || !findPath(0, 0, 0, m)){
         // std::vector<std::string> noSolution;
         // noSolution.push_back("no solution found.");
         // return noSolution;
@@ -97,17 +102,10 @@ std::vector<std::string> Pathfinder::solveMaze() {
         // std::cout << "found a path!" << std::endl;
         return solutionPath;
     }
-    for(int i = 0; i < std::pow(MAZE_SIZE, 3); i++){
-        // maze[i%MAZE_SIZE][(i/MAZE_SIZE)%MAZE_SIZE][(i/int(std::pow(MAZE_SIZE, 2)))%MAZE_SIZE] = FREE;
-        if(maze[X(i, MAZE_SIZE)][Y(i, MAZE_SIZE)][Z(i, MAZE_SIZE)] == ON_PATH){
-            maze[X(i, MAZE_SIZE)][Y(i, MAZE_SIZE)][Z(i, MAZE_SIZE)] == FREE;
-        }
-    
-    }
     
 }
 
-bool Pathfinder::findPath(int x, int y, int z){
+bool Pathfinder::findPath(int x, int y, int z, int m[MAZE_SIZE][MAZE_SIZE][MAZE_SIZE]){
     // std::cout << "testing coordinate: (" << x << ", " << y << ", " << z << ")" << std::endl;
     // std::cout << "\tvalue at coord: " << maze[x][y][z] << std::endl;
 
@@ -123,18 +121,18 @@ bool Pathfinder::findPath(int x, int y, int z){
         solutionPath.pop_back();
         return false;
     }
-    if(maze[x][y][z] == ON_PATH || maze[x][y][z] == OBSTACLE){
+    if(m[x][y][z] == ON_PATH || m[x][y][z] == OBSTACLE){
         solutionPath.pop_back();
         return false;
     }
 
-    maze[x][y][z] = ON_PATH;
+    m[x][y][z] = ON_PATH;
 
     if(x == MAZE_SIZE-1 && y == MAZE_SIZE-1 && z == MAZE_SIZE-1){
         return true;
     }
 
-    if(findPath(x+1, y, z) || findPath(x-1, y, z) || findPath(x, y+1, z) || findPath(x, y-1, z) || findPath(x, y, z+1) || findPath(x, y, z-1)){
+    if(findPath(x+1, y, z, m) || findPath(x-1, y, z, m) || findPath(x, y+1, z, m) || findPath(x, y-1, z, m) || findPath(x, y, z+1, m) || findPath(x, y, z-1, m)){
         return true; // meaning that we have not hit a dead end.
     }else{
         solutionPath.pop_back();
